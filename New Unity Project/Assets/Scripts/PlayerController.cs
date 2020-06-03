02f3fdd7426d60da428e89965f694a0d1b297ce4
Alec Erno <aleclee1996@gmail.com>
2020-06-02 23:57:43 -0500

    Movement added

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -15,6 +15,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if()
+        if(Input.GetAxisRaw("Horizontal")>0.0f || Input.GetAxisRaw("Horizontal")<0.0f)
+        {
+            transform.Translate(new Vector3(Input.GetAxisRaw("Horizontal")*moveSpeed*Time.deltaTime, 0f, 0f));
+        }
+        if(Input.GetAxisRaw("Vertical")>0.0f || Input.GetAxisRaw("Vertical")<0.0f)
+        {
+            transform.Translate(new Vector3(0f, Input.GetAxisRaw("Vertical")*moveSpeed*Time.deltaTime, 0f));
+        }
     }
 }
